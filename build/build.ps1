6b1c97ea0362cffafcd789aab2a6280691352435
Lacro59 <lacro59@gmail.com>
2021-09-08 18:39:30 +0200

    updated config

---
+++ build/build.ps1
@@ -0,0 +1,22 @@
+param(
+	[string]$ConfigurationName, 
+	[string]$OutDir
+)
+
+
+$PlaynitePath = "D:\Playnite_dev"
+$ToolboxPath = (Join-Path $PlaynitePath "toolbox.exe")
+$OutDirPath = (Join-Path $OutDir "..")
+
+
+if ($ConfigurationName -eq "release") 
+{
+	if (Test-Path $ToolboxPath)
+	{
+		& $ToolboxPath "pack" $OutDir $OutDirPath
+	}
+	else 
+	{
+		Write-Error "toolbox.exe not find."
+	}	
+}
