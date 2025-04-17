45e58bd77f5af836cfd3034b5ede7d747aa4808e
Lacro59 <lacro59@gmail.com>
2025-04-17 14:18:16 +0200

    Update GenshinImpactAchievements.cs

--- source/Clients/GenshinImpactAchievements.cs
+++ source/Clients/GenshinImpactAchievements.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using CommonPlayniteShared.Common;
 using CommonPluginsShared;
 using Playnite.SDK;
 using Playnite.SDK.Data;
@@ -21,6 +23,7 @@ namespace SuccessStory.Clients
         private static string UrlAchievements => Url + @"/ExcelBinOutput/AchievementExcelConfigData.json";
 
         private static string PaimonMoe_Url => "https://raw.githubusercontent.com/MadeBaruna/paimon-moe/main";
+        private static string PaimonMoe_UrlApi => "https://api.github.com/repos/MadeBaruna/paimon-moe";
         private static string PaimonMoe_UrlSource => "https://github.com//MadeBaruna/paimon-moe";
         private static string PaimonMoe_UrlAchievements => PaimonMoe_Url + "/src/data/achievement/{0}.json";
         #endregion
@@ -39,23 +42,42 @@ namespace SuccessStory.Clients
 
             try
             {
-                string url = string.Format(PaimonMoe_UrlAchievements, LocalLang.ToLower());
-                string textMapString = Web.DownloadStringData(url).GetAwaiter().GetResult();
-                _ = Serialization.TryFromJson(textMapString, out dynamic TextMap);
-                if (TextMap == null)
+                string url = PaimonMoe_UrlApi;
+                string reponse = Web.DownloadStringData(url).GetAwaiter().GetResult();
+                _ = Serialization.TryFromJson(reponse, out dynamic gitHub);
+                if (gitHub == null)
                 {
-                    url = string.Format(PaimonMoe_UrlAchievements, "en");
-                    textMapString = Web.DownloadStringData(url).GetAwaiter().GetResult();
-                    _ = Serialization.TryFromJson(textMapString, out TextMap);
-                    if (TextMap == null)
-                    {
-                        throw new Exception($"No data from {url}");
-                    }
+                    throw new Exception($"No data with {url}");
                 }
+                string branch = gitHub["default_branch"];
+                DateTime pushed_at = gitHub["pushed_at"];
+                string pushedAt = pushed_at.ToString("yyyy-MM-dd_HH-mm-ss");
+
+
+                string cachePath = Path.Combine(PluginDatabase.Paths.PluginCachePath, "Genshin_" + pushedAt, "TextMap.json");
+                _ = Serialization.TryFromJsonFile(cachePath, out dynamic textMap);
+                if (textMap == null)
+                {
+                    url = string.Format(PaimonMoe_UrlAchievements, LocalLang.ToLower());
+                    string textMapString = Web.DownloadStringData(url).GetAwaiter().GetResult();
+                    _ = Serialization.TryFromJson(textMapString, out textMap);
+                    if (textMap == null)
+                    {
+                        url = string.Format(PaimonMoe_UrlAchievements, "en");
+                        textMapString = Web.DownloadStringData(url).GetAwaiter().GetResult();
+                        _ = Serialization.TryFromJson(textMapString, out textMap);
+                        if (textMap == null)
+                        {
+                            throw new Exception($"No data from {url}");
+                        }
+                    }
+                    FileSystem.WriteStringToFile(cachePath, textMapString);
+                }
+
 
                 for (int i = 0; i < 80; i++)
                 {
-                    string map = Serialization.ToJson(TextMap[i.ToString()]);
+                    string map = Serialization.ToJson(textMap[i.ToString()]);
                     if (Serialization.TryFromJson(map, out Data data) && data != null)
                     {
                         int categoryOrder = data.Order;
@@ -115,62 +137,71 @@ namespace SuccessStory.Clients
         }
 
 
-        public bool ImportAchievements(Game game)
+        public void ImportAchievements(Game game)
         {
-            string path = API.Instance.Dialogs.SelectFile("JSON (.json)|*.json");
-            if (!string.IsNullOrWhiteSpace(path)  && path.EndsWith(".json", StringComparison.InvariantCultureIgnoreCase))
+            GlobalProgressOptions options = new GlobalProgressOptions(ResourceProvider.GetString("LOCCommonImporting"))
             {
-                bool done = false;
-                GameAchievements gameAchievements = GetAchievements(game);
+                Cancelable = true,
+                IsIndeterminate = false
+            };
 
-                if (Serialization.TryFromJsonFile(path, out PaimonMoeLocalData paimonMoeLocalData))
+            _ = API.Instance.Dialogs.ActivateGlobalProgress((a) =>
+            {
+                string path = API.Instance.Dialogs.SelectFile("JSON (.json)|*.json");
+                if (!string.IsNullOrWhiteSpace(path) && path.EndsWith(".json", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    if (paimonMoeLocalData?.Achievement != null)
+                    bool done = false;
+                    GameAchievements gameAchievements = GetAchievements(game);
+
+                    if (Serialization.TryFromJsonFile(path, out PaimonMoeLocalData paimonMoeLocalData))
                     {
-                        paimonMoeLocalData.Achievement.ForEach(x =>
+                        if (paimonMoeLocalData?.Achievement != null)
                         {
-                            x.Value.ForEach(y =>
+                            paimonMoeLocalData.Achievement.ForEach(x =>
                             {
-                                Achievement item = gameAchievements.Items.FirstOrDefault(z => z.ApiName == y.Key && y.Value);
-                                if (item != null)
+                                x.Value.ForEach(y =>
                                 {
-                                    item.DateUnlocked = new DateTime(1982, 12, 15, 0, 0, 0, 0);
+                                    Achievement item = gameAchievements.Items.FirstOrDefault(z => z.ApiName == y.Key && y.Value);
+                                    if (item != null)
+                                    {
+                                        item.DateUnlocked = new DateTime(1982, 12, 15, 0, 0, 0, 0);
+                                    }
+                                });
+                            });
+                            done = true;
+                        }
+                    }
+
+                    if (Serialization.TryFromJsonFile(path, out SeelieMeLocalData seelieMeLocalData))
+                    {
+                        if (seelieMeLocalData?.Achievements != null)
+                        {
+                            seelieMeLocalData.Achievements.ForEach(x =>
+                            {
+                                if (x.Value.Done)
+                                {
+                                    Achievement item = gameAchievements.Items.FirstOrDefault(z => z.ApiName == x.Key);
+                                    if (item != null)
+                                    {
+                                        item.DateUnlocked = new DateTime(1982, 12, 15, 0, 0, 0, 0);
+                                    }
                                 }
                             });
-                        });
-                        done = true;
+                            done = true;
+                        }
                     }
-                }
 
-                if (Serialization.TryFromJsonFile(path, out SeelieMeLocalData seelieMeLocalData))
-                {
-                    if (seelieMeLocalData?.Achievements != null)
+                    if (done)
                     {
-                        seelieMeLocalData.Achievements.ForEach(x =>
-                        {
-                            if (x.Value.Done)
-                            {
-                                Achievement item = gameAchievements.Items.FirstOrDefault(z => z.ApiName == x.Key);
-                                if (item != null)
-                                {
-                                    item.DateUnlocked = new DateTime(1982, 12, 15, 0, 0, 0, 0);
-                                }
-                            }
-                        });
-                        done = true;
+                        PluginDatabase.Update(gameAchievements);
+                        _ = API.Instance.Dialogs.ShowMessage(ResourceProvider.GetString("LOCImportCompleted"), ResourceProvider.GetString("LOCSuccessStory"));
+                    }
+                    else
+                    {
+                        _ = API.Instance.Dialogs.ShowErrorMessage(ResourceProvider.GetString("LOCImportError"), ResourceProvider.GetString("LOCSuccessStory"));
                     }
                 }
-
-                if (done)
-                {
-                    PluginDatabase.Update(gameAchievements);
-                    _ = API.Instance.Dialogs.ShowMessage(ResourceProvider.GetString("LOCImportCompleted"), ResourceProvider.GetString("LOCSuccessStory"));
-                    return true;
-                }
-            }
-
-            _ = API.Instance.Dialogs.ShowErrorMessage(ResourceProvider.GetString("LOCImportError"), ResourceProvider.GetString("LOCSuccessStory"));
-            return false;
+            }, options);
         }
 
 
