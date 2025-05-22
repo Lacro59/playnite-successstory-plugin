61239ae2d551de3d1d3f6616bb591f315e7ddcf1
Lacro59 <lacro59@gmail.com>
2025-05-22 08:45:22 +0200

    Update SuccessStoryEditManual.xaml.cs

--- source/Views/SuccessStoryEditManual.xaml.cs
+++ source/Views/SuccessStoryEditManual.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using CommonPluginsShared;
 using CommonPluginsShared.Extensions;
-using Playnite.SDK;
 using Playnite.SDK.Data;
 using Playnite.SDK.Models;
 using SuccessStory.Models;
@@ -8,19 +7,12 @@ using SuccessStory.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using TemperatureMeasurementTool;
 
 namespace SuccessStory.Views
