affa29f2f43182709e38cdb84b5166ee29b73331
Lacro59 <lacro59@gmail.com>
2025-10-04 14:04:55 +0200

    Update SuccessStorySettings.cs

--- source/SuccessStorySettings.cs
+++ source/SuccessStorySettings.cs
@@ -9,7 +9,6 @@ using System;
 using System.Linq;
 using Playnite.SDK.Models;
 using CommonPluginsShared.Plugins;
-using CommonPluginsStores;
 using CommonPluginsStores.Models;
 
 namespace SuccessStory
